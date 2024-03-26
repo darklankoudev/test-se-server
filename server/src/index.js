d37f7dff14cd2c54ad44e19b9afeae1339254a02
thanhphm <you@example.com>
2024-03-26 11:29:32 +0700

    server

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const functions = require("firebase-functions");
 const axios = require('axios');
 const cors = require('cors'); 
 const app = express();
@@ -50,10 +49,6 @@ app.get('/api/proposal/detail/:id', async (req, res) => {
 });
 
 
-
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-// exports.app = functions.https.onRequest(app);
--- src/constants/constants.js
+++
@@ -1,8 +0,0 @@
-const API_BASE_URL = "https://it.api.namada.red/api/v1/chain";
-const API_BASE_URL_SUPPORT = "https://namada-explorer-api.stakepool.dev.br/node";
-const RPC_THANHPHM_URL = "https://rpc-namada-se-dev-thanhphm.uk"
-const API_VLVN_URL = "https://indexer.validatorvn.com"
-const API_DETAIL_VALIDATOR = "https://namada.api.explorers.guru/api/validators"
-const API_PROPOSAL_BASE = "https://namada-indexer.kintsugi-nodes.com"
-
-export { API_BASE_URL, API_BASE_URL_SUPPORT, RPC_THANHPHM_URL, API_VLVN_URL, API_DETAIL_VALIDATOR, API_PROPOSAL_BASE};
